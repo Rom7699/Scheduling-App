5c5aa0032bed512d879031fd2b867caa8d7bffb4
Rom7699 <rom7699@gmail.com>
2025-04-13 19:28:58 +0300

    added the server deployment url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5500',
+        target: 'https://scheduling-app-server.onrender.com',
         changeOrigin: true,
         secure: false,
       }
